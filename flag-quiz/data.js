cdc7399b85a340b8d6f48e25f3341423cc75d120
tsunomaru21 <shooting.star.aquarius.0121@gmail.com>
2026-01-22 06:53:22 +0900

    Add files via upload

---
+++ flag-quiz/data.js
@@ -0,0 +1,89 @@
+// data.js
+
+// ============
+// レベル1（実データ10問入り）
+// ============
+const levels = {
+    level1: [
+      {
+        country: "Japan",
+        flag: "https://flagcdn.com/w320/jp.png",
+        hint1: "アジアのしまのくに",
+        hint2: "さくらやすしがゆうめい",
+        answer: "アジアのひがしがわにあるよ"
+      },
+      {
+        country: "United States",
+        flag: "https://flagcdn.com/w320/us.png",
+        hint1: "きたアメリカにあるおおきなくに",
+        hint2: "ハンバーガーがにんき",
+        answer: "きたアメリカのまんなかあたりだよ"
+      },
+      {
+        country: "South Korea",
+        flag: "https://flagcdn.com/w320/kr.png",
+        hint1: "アジアのとなりのくに",
+        hint2: "K-POPがゆうめい",
+        answer: "にほんのにしがわにあるよ"
+      },
+      {
+        country: "China",
+        flag: "https://flagcdn.com/w320/cn.png",
+        hint1: "アジアのとてもおおきなくに",
+        hint2: "ぎょうざやパンダがゆうめい",
+        answer: "にほんのたいりくがわだよ"
+      },
+      {
+        country: "France",
+        flag: "https://flagcdn.com/w320/fr.png",
+        hint1: "ヨーロッパのくに",
+        hint2: "エッフェルとうがあるよ",
+        answer: "ヨーロッパのにしがわあたりだよ"
+      },
+      {
+        country: "United Kingdom",
+        flag: "https://flagcdn.com/w320/gb.png",
+        hint1: "ヨーロッパのしまのくに",
+        hint2: "えいごのくに",
+        answer: "ヨーロッパのにしにあるしま"
+      },
+      {
+        country: "Germany",
+        flag: "https://flagcdn.com/w320/de.png",
+        hint1: "ヨーロッパのまんなかのくに",
+        hint2: "ソーセージがにんき",
+        answer: "フランスのとなりだよ"
+      },
+      {
+        country: "Italy",
+        flag: "https://flagcdn.com/w320/it.png",
+        hint1: "ヨーロッパのブーツみたいなくに",
+        hint2: "ピザとパスタ",
+        answer: "ヨーロッパのなんぶだよ"
+      },
+      {
+        country: "Australia",
+        flag: "https://flagcdn.com/w320/au.png",
+        hint1: "オセアニアのしまのくに",
+        hint2: "コアラやカンガルーがいるよ",
+        answer: "ちきゅうのしたのほう（みなみ）だよ"
+      },
+      {
+        country: "Brazil",
+        flag: "https://flagcdn.com/w320/br.png",
+        hint1: "みなみアメリカのくに",
+        hint2: "サッカーがとてもつよい",
+        answer: "みなみアメリカのひがしがわだよ"
+      }
+    ],
+  
+    // =============
+    // レベル2〜6（後でデータを充填）
+    // =============
+    level2: [],
+    level3: [],
+    level4: [],
+    level5: [],
+    level6: []
+  };
+  
