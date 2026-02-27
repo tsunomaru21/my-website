2e3831e6071448e001d74dc73dadfa14380ec3ef
tsunomaru21 <shooting.star.aquarius.0121@gmail.com>
2026-02-27 14:49:22 +0900

    Add files via upload

---
+++ apli1.js
@@ -0,0 +1,33 @@
+﻿$(function(){
+	$("#title").css("color","blue")
+});	
+
+$(function(){
+	$(".clear").click(function(){
+		$("li:hidden").show();
+	});	
+});
+
+$(function(){
+	$(".simokitazawa1").click(function(){
+		$("li").not(".simokitazawa").hide()
+	});
+});		
+
+$(function(){
+	$(".tyouhu1").click(function(){
+		$("li").not(".tyouhu").hide()
+	});
+});		
+
+$(function(){
+	$(".shinnjuku1").click(function(){
+		$("li").not(".shinnjuku").hide()
+	});
+});		
+
+$(function(){
+	$(".tokyo1").click(function(){
+		$("li").not(".tokyo").hide()
+	});
+});		
